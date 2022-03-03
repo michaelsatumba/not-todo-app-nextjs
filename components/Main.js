fc93976bf527b45eab285701538a108fb40ee08e
michaelsatumba <mjsatumba@gmail.com>
2022-03-03 14:28:05 -0800

    flexbox is confusing

--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import React, { useState } from 'react';
 import Third from '../components/third';
+import xSymbol from '../public/icon-cross.svg';
 
 function Main() {
 	const [items, setItems] = useState([]);
 	const [notTodo, setNotTodo] = useState('');
 	// const [cross, setCross] = useState(true);
-	const complete = true ? 'line-through' : 'none';
+	const complete = false ? 'line-through' : 'none';
 
 	// const list = ['Watch TV', 'hello'];
 
@@ -38,6 +39,10 @@ function Main() {
 		// setItems(temporaryTodos);
 	};
 
+	const clear = () => {
+		setItems([]); // sets array to empty
+	};
+
 	return (
 		<div className="grid justify-items-center">
 			{/* first */}
@@ -63,16 +68,32 @@ function Main() {
 
 			{/* second */}
 			<div className="flex flex-col border-none bg-slate-700 px-12 py-2 text-white">
-				<div className="flex border-b-2 ">
-					<ul className="">
+				<div className="border-b-2">
+					<ul className="flex flex-col">
 						{items.map((e, y) => (
-							<li className={`space-x-40 text-green-500 ${complete}`} key={y}>
+							<li
+								className={`flex flex-row justify-between text-green-500 ${complete}`}
+								key={y}
+							>
 								<input
 									type="checkbox"
-									className=""
+									className="w-5 h-5 place-self-center"
 									onClick={() => crossOff(y)}
 								/>
-								{e} <button onClick={() => deleteItem(y)}>X</button>
+								{e}
+								<button
+									className="place-self-center"
+									onClick={() => deleteItem(y)}
+								>
+									<div className="h-5 w-5 relative">
+										<Image
+											src={xSymbol}
+											alt="xSymbol"
+											objectFit="cover"
+											layout="fill"
+										/>
+									</div>
+								</button>
 							</li>
 						))}
 					</ul>
@@ -89,9 +110,9 @@ function Main() {
 					</div> */}
 				</div>
 
-				<div className="flex justify-between text-gray-400">
-					<p>5 items left</p>
-					<p>Clear Completed</p>
+				<div className="flex space-x-24 text-gray-400">
+					<p>{items.length} items left</p>
+					<button onClick={clear}>Clear Completed</button>
 				</div>
 			</div>
 
